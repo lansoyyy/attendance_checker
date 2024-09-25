215237eb69155ca519386b53a8e12a7c2391b3d6
Lans12345 <olanalans12345@gmail.com>
2024-09-25 18:50:00 +0800

    wip qr screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_checker/screens/qr_screen.dart';
 import 'package:attendance_checker/utils/colors.dart';
 import 'package:attendance_checker/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
@@ -93,59 +94,66 @@ class HomeScreen extends StatelessWidget {
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3),
               itemBuilder: (context, index) {
-                return Card(
-                  color: secondary,
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          TextWidget(
-                            text: 'Subject',
-                            fontSize: 18,
-                            color: primary,
-                            fontFamily: 'Bold',
-                          ),
-                          TextWidget(
-                            text: 'Name/Code',
-                            fontSize: 18,
-                            color: primary,
-                            fontFamily: 'Bold',
-                          ),
-                          const SizedBox(
-                            height: 3,
-                          ),
-                          TextWidget(
-                            text: 'Tue-Fri',
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontFamily: 'Bold',
-                          ),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                TextWidget(
-                                  text: '7:30AM',
-                                  fontSize: 12,
-                                  color: Colors.black,
-                                  fontFamily: 'Bold',
-                                ),
-                                TextWidget(
-                                  text: '10:30AM',
-                                  fontSize: 12,
-                                  color: Colors.black,
-                                  fontFamily: 'Bold',
-                                ),
-                              ],
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const QRScreen()),
+                    );
+                  },
+                  child: Card(
+                    color: secondary,
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            TextWidget(
+                              text: 'Subject',
+                              fontSize: 18,
+                              color: primary,
+                              fontFamily: 'Bold',
                             ),
-                          ),
-                        ],
+                            TextWidget(
+                              text: 'Name/Code',
+                              fontSize: 18,
+                              color: primary,
+                              fontFamily: 'Bold',
+                            ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            TextWidget(
+                              text: 'Tue-Fri',
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontFamily: 'Bold',
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  TextWidget(
+                                    text: '7:30AM',
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                    fontFamily: 'Bold',
+                                  ),
+                                  TextWidget(
+                                    text: '10:30AM',
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                    fontFamily: 'Bold',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
---
+++ lib/screens/qr_screen.dart
@@ -0,0 +1,16 @@
+import 'package:attendance_checker/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class QRScreen extends StatelessWidget {
+  const QRScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: primary,
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
