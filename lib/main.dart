273bf719ef22ea819d5dd885e5079f2897cdc2fc
Lans12345 <olanalans12345@gmail.com>
2024-09-27 06:49:57 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:attendance_checker/firebase_options.dart';
 import 'package:attendance_checker/screens/auth/initial_landing_page.dart';
 import 'package:attendance_checker/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'eats-6c0dc',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
