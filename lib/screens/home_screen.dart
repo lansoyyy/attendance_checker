2e5e0486fbaa9042200a12ec2334880121f34ae7
Lans12345 <olanalans12345@gmail.com>
2024-10-03 07:50:00 +0800

    added student func

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,12 +2,22 @@ import 'package:attendance_checker/screens/qr_screen.dart';
 import 'package:attendance_checker/utils/colors.dart';
 import 'package:attendance_checker/utils/const.dart';
 import 'package:attendance_checker/widgets/text_widget.dart';
+import 'package:attendance_checker/widgets/toast_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String myname = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +35,7 @@ class HomeScreen extends StatelessWidget {
               return const Center(child: CircularProgressIndicator());
             }
             dynamic data = snapshot.data;
+            myname = '${data['fname']} ${data['lname']}';
             return Column(
               children: [
                 Stack(
@@ -105,114 +116,140 @@ class HomeScreen extends StatelessWidget {
                   color: primary,
                   fontFamily: 'Bold',
                 ),
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3),
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => QRScreen(
-                                      data: const {},
-                                    )),
-                          );
-                        },
-                        child: Card(
-                          color: secondary,
-                          child: SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  TextWidget(
-                                    text: 'Subject',
-                                    fontSize: 18,
-                                    color: primary,
-                                    fontFamily: 'Bold',
-                                  ),
-                                  TextWidget(
-                                    text: 'Name/Code',
-                                    fontSize: 18,
-                                    color: primary,
-                                    fontFamily: 'Bold',
-                                  ),
-                                  const SizedBox(
-                                    height: 3,
-                                  ),
-                                  TextWidget(
-                                    text: 'Tue-Fri',
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontFamily: 'Bold',
-                                  ),
-                                  Align(
-                                    alignment: Alignment.bottomRight,
+                StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('Class')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        print(snapshot.error);
+                        return const Center(child: Text('Error'));
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Padding(
+                          padding: EdgeInsets.only(top: 50),
+                          child: Center(
+                              child: CircularProgressIndicator(
+                            color: Colors.black,
+                          )),
+                        );
+                      }
+
+                      final data = snapshot.requireData;
+                      return Expanded(
+                        child: GridView.builder(
+                          itemCount: data.docs.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3),
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (context) => QRScreen(
+                                            data: data.docs[index],
+                                          )),
+                                );
+                              },
+                              child: Card(
+                                color: secondary,
+                                child: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
                                     child: Column(
                                       crossAxisAlignment:
-                                          CrossAxisAlignment.end,
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
                                       children: [
                                         TextWidget(
-                                          text: '7:30AM',
-                                          fontSize: 12,
-                                          color: Colors.black,
+                                          text: data.docs[index]['subname'],
+                                          fontSize: 18,
+                                          color: primary,
                                           fontFamily: 'Bold',
                                         ),
+                                        const SizedBox(
+                                          height: 3,
+                                        ),
                                         TextWidget(
-                                          text: '10:30AM',
-                                          fontSize: 12,
-                                          color: Colors.black,
+                                          text: data.docs[index]['schedule'],
+                                          fontSize: 16,
+                                          color: Colors.white,
                                           fontFamily: 'Bold',
+                                        ),
+                                        Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              TextWidget(
+                                                text: data.docs[index]
+                                                    ['timestart'],
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                                fontFamily: 'Bold',
+                                              ),
+                                              TextWidget(
+                                                text: data.docs[index]
+                                                    ['timeend'],
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                                fontFamily: 'Bold',
+                                              ),
+                                            ],
+                                          ),
                                         ),
                                       ],
                                     ),
                                   ),
-                                ],
+                                ),
                               ),
-                            ),
-                          ),
+                            );
+                          },
                         ),
                       );
-                    },
-                  ),
-                ),
+                    }),
                 Align(
                   alignment: Alignment.bottomCenter,
                   child: Container(
                     width: double.infinity,
                     height: 100,
                     decoration: const BoxDecoration(color: primary),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                              color: Colors.white, shape: BoxShape.circle),
-                          child: const Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: Icon(
-                              Icons.qr_code_scanner_sharp,
-                              color: primary,
-                              size: 40,
+                    child: GestureDetector(
+                      onTap: () {
+                        scanQRCode();
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                                color: Colors.white, shape: BoxShape.circle),
+                            child: const Padding(
+                              padding: EdgeInsets.all(15.0),
+                              child: Icon(
+                                Icons.qr_code_scanner_sharp,
+                                color: primary,
+                                size: 40,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        TextWidget(
-                          text: 'Scan',
-                          fontSize: 12,
-                          color: Colors.white,
-                          fontFamily: 'Bold',
-                        ),
-                      ],
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextWidget(
+                            text: 'Scan',
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontFamily: 'Bold',
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -220,5 +257,52 @@ class HomeScreen extends StatelessWidget {
             );
           }),
     );
+  }
+
+  String qrCode = 'Unknown';
+
+  Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return const AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircularProgressIndicator(),
+              ],
+            ),
+          );
+        },
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.qrCode = qrCode;
+      });
+      await FirebaseFirestore.instance.collection('Class').doc(qrCode).update({
+        'students': FieldValue.arrayUnion([
+          {
+            'name': myname,
+            'time': DateFormat('h:mm a').format(DateTime.now()),
+            'date': DateFormat('dd/MM/yyyy').format(DateTime.now()),
+          }
+        ])
+      });
+      Navigator.pop(context);
+      showToast('Attendance recorded!');
+    } on PlatformException {
+      qrCode = 'Failed to get platform version.';
+    }
   }
 }
