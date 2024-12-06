444aaf765226266d8b664fd92272209132c7cfd5
Lans12345 <olanalans12345@gmail.com>
2024-12-06 19:03:41 +0800

    wip class invitation func

--- lib/screens/qr_screen.dart
+++ lib/screens/qr_screen.dart
@@ -1,7 +1,10 @@
 import 'package:attendance_checker/utils/colors.dart';
 import 'package:attendance_checker/widgets/text_widget.dart';
+import 'package:attendance_checker/widgets/toast_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:intl/intl.dart' show DateFormat, toBeginningOfSentenceCase;
 
 class QRScreen extends StatelessWidget {
   dynamic data;
@@ -10,6 +13,16 @@ class QRScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          showStudents(context);
+        },
+      ),
       backgroundColor: secondary,
       body: SafeArea(
         child: SingleChildScrollView(
@@ -134,6 +147,131 @@ class QRScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  final searchController = TextEditingController();
+  String nameSearched = '';
+  showStudents(context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: StatefulBuilder(builder: (context, setState) {
+            return SizedBox(
+              height: 300,
+              width: 500,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+                        child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 0.5,
+                              ),
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: TextFormField(
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Regular',
+                                  fontSize: 14),
+                              onChanged: (value) {
+                                setState(() {
+                                  nameSearched = value;
+                                });
+                              },
+                              decoration: const InputDecoration(
+                                  labelStyle: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                  hintText: 'Search Student',
+                                  hintStyle: TextStyle(
+                                      fontFamily: 'Regular', fontSize: 18),
+                                  prefixIcon: Icon(
+                                    Icons.search,
+                                    color: Colors.grey,
+                                  )),
+                              controller: searchController,
+                            ),
+                          ),
+                        ),
+                      ),
+                      StreamBuilder<QuerySnapshot>(
+                          stream: FirebaseFirestore.instance
+                              .collection('Student')
+                              .where('fname',
+                                  isGreaterThanOrEqualTo:
+                                      toBeginningOfSentenceCase(nameSearched))
+                              .where('fname',
+                                  isLessThan:
+                                      '${toBeginningOfSentenceCase(nameSearched)}z')
+                              .snapshots(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<QuerySnapshot> snapshot) {
+                            if (snapshot.hasError) {
+                              print(snapshot.error);
+                              return const Center(child: Text('Error'));
+                            }
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Padding(
+                                padding: EdgeInsets.only(top: 50),
+                                child: Center(
+                                    child: CircularProgressIndicator(
+                                  color: Colors.black,
+                                )),
+                              );
+                            }
+
+                            final studentData = snapshot.requireData;
+                            return Column(
+                              children: [
+                                for (int i = 0;
+                                    i < studentData.docs.length;
+                                    i++)
+                                  ListTile(
+                                    onTap: () async {
+                                      Navigator.pop(context);
+                                      await FirebaseFirestore.instance
+                                          .collection('Class')
+                                          .doc(data.id)
+                                          .update({
+                                        'students': FieldValue.arrayUnion([
+                                          studentData.docs[i].id,
+                                        ])
+                                      });
+                                      showToast('Student added to this class.');
+                                    },
+                                    leading: const Icon(
+                                      Icons.account_circle_rounded,
+                                      size: 35,
+                                    ),
+                                    title: TextWidget(
+                                      text:
+                                          '${studentData.docs[i]['fname']} ${studentData.docs[i]['lname']}',
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                              ],
+                            );
+                          }),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+        );
+      },
     );
   }
 }
